4547e6c4b867667912614861cf69888532108703
cfunch29 <cfunch29@gmail.com>
2025-12-04 19:36:35 -0500

    first try

--- script.js
+++ script.js
@@ -5,3 +5,18 @@
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
 
+for (let x= 1; x <= 100; x++){
+    console.log(x);
+    if (x % 3 == 0 && x % 5 == 0);{
+    console.log("Fizz Buzz")
+    }
+    else if (x % 3 !==0 || x % 5 !==0);{
+        console.log(x)
+    }
+    else if (x % 3 ==0);{
+        console.log("Fizz")
+    }
+    else (x % 5 ==0);{
+        console.log("Buzz")
+    }
+}
