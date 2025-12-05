23516f1ae51c8e8dff765150bbdbd1843994199b
cfunch29 <cfunch29@gmail.com>
2025-12-05 01:46:37 -0500

    print the data

---
+++ CSVLoopy.js
@@ -0,0 +1,10 @@
+let csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+    console.log(csv);{
+        for (let x = 0; x < csv.length; x++)
+            if (csv[x] == "/n"){
+                continue;
+
+            }
+    }
+
+    let row = ","
