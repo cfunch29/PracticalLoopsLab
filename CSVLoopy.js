676308dba9a7d05983eaa7025a001dd64156e5bf
cfunch29 <cfunch29@gmail.com>
2025-12-09 16:33:55 -0500

    make sure to delete previous console logs so the rest of the code will print. end result.

--- CSVLoopy.js
+++ CSVLoopy.js
@@ -1,4 +1,4 @@
-let str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 // console.log(str);
 
 // console.log(cell1, cell2, cell3, cell4);
@@ -21,12 +21,11 @@ for (let i = 0; i < str.length; i++) {
 
     if (current == ",") {
         // move to the next cell
-        console.log("Comma");
         commas++
     }
     else if (current == "\n") {
         // Print all cells
-        console.log("New Row");
+        console.log(cell1, cell2, cell3, cell4);
         commas = 0;
         cell1 = "";
         cell2 = "";
