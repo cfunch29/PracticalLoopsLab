21f0fb90a82f66192031dfce9088d0d3701e4fa3
cfunch29 <cfunch29@gmail.com>
2025-12-09 14:37:57 -0500

    changed variable

--- CSVLoopy.js
+++ CSVLoopy.js
@@ -1,10 +1,15 @@
-let csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
-    console.log(csv);{
-        for (let x = 0; x < csv.length; x++)
-            if (csv[x] == "/n"){
+let str = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
+    console.log(str);
+    {
+        for (let x = 0; x < str.length; x++)
+            if (str[x] == "/n"){
                 continue;
 
             }
     }
 
-    let row = ","
+
+    let cell1 = " ";
+    let cell2 = " ";
+    let cell3 = " ";
+    let cell4 = " ";
